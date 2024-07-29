ec583da9aaebac1c4cf57173535d0bd296933af4
Henry Lee <henryleeworld@gmail.com>
2024-07-30 00:06:32 +0800

    feat:更新最新資料

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -7,7 +7,7 @@
         <meta name="description" content="" />
         <title>{{ config('app.name', 'Laravel') }}</title>
         @vite(['resources/sass/app.scss'])
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <link href="{{ asset('css/sb-admin-2.min.css') }}" rel="stylesheet" />
     </head>
     <body class="bg-gradient-primary">
